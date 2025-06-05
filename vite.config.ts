78c8e655ca4db19e6c0260be3a91532e94a10cec
Marnitz Malan <marnitz.malan@payfast.co.za>
2025-06-05 15:34:26 +0200

    build configs

--- vite.config.ts
+++ vite.config.ts
@@ -12,5 +12,11 @@ export default defineConfig({
 		host: '0.0.0.0',
 		port: Number(process.env.PORT) || 3000,
 		strictPort: true
+	},
+	optimizeDeps: {
+		exclude: ['@sveltejs/kit']
+	},
+	ssr: {
+		noExternal: ['@sveltejs/kit']
 	}
 });
