c69264b53b1a104252aa6284101960fa92f5d1b3
Marnitz Malan <marnitz.malan@payfast.co.za>
2025-06-05 15:26:49 +0200

    build configs

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,12 @@ export default defineConfig({
 	plugins: [sveltekit()],
 	server: {
 		host: '0.0.0.0',
-		port: Number(process.env.PORT) || 3000
+		port: Number(process.env.PORT) || 3000,
+		strictPort: true
+	},
+	preview: {
+		host: '0.0.0.0',
+		port: Number(process.env.PORT) || 3000,
+		strictPort: true
 	}
 });
