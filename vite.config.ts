1a18404992bb2e88f3d2f310d0056a2888e6dc41
Marnitz Malan <marnitz.malan@payfast.co.za>
2025-06-06 12:56:30 +0200

    build configs

--- vite.config.ts
+++ vite.config.ts
@@ -4,16 +4,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
 	plugins: [sveltekit()],
 	server: {
-		host: '0.0.0.0',
-		port: Number(process.env.PORT) || 3000,
-		strictPort: true,
-		allowedHosts: ['portfolio-57kp.onrender.com', 'localhost']
-	},
-	preview: {
-		host: '0.0.0.0',
-		port: Number(process.env.PORT) || 3000,
-		strictPort: true,
-		allowedHosts: ['portfolio-57kp.onrender.com', 'localhost']
+		allowedHosts: ['marnitzmalan.com', 'www.marnitzmalan.com']
 	},
 	optimizeDeps: {
 		exclude: ['@sveltejs/kit']
